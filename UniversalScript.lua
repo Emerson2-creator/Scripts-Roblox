9345d85ae782325f79d5442e9f126b77e727f9ce
Emerson Jose Boito Neto <josenetoboito@gmail.com>
2025-02-23 11:27:06 +0100

    Update UniversalScript.lua

--- UniversalScript.lua
+++ UniversalScript.lua
@@ -1,1 +1,1 @@
-Loadstring(game:HttpGet("https://raw.githubusercontent.com/Emerson2-creator/Universal-Gui-/refs/heads/main/Raw-Code/UniversalGuiScript.lua"))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Emerson2-creator/Universal-Gui-/refs/heads/main/Raw-Code/UniversalGuiScript.lua'))()
